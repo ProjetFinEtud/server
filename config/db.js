b912cce15812dd079dfd36738cafbd60592d64ff
Abdoulaye BERTE <abdouberte97@gmail.com>
2024-03-10 19:59:32 +0100

    update lowercase

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 
 const { Sequelize } = require("sequelize");
 const dbConfig = require("./db");
-
+console.log("variable d'environnement ! ")
 console.log(process.env.DB)
 
 const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
