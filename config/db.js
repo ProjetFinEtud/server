7ecaa885c2c59b9f8169ed0776097595d4d1f6d9
Abdoulaye BERTE <abdouberte97@gmail.com>
2024-03-10 19:57:46 +0100

    update lowercase

--- config/db.js
+++ config/db.js
@@ -1,4 +1,9 @@
+
+
 const { Sequelize } = require("sequelize");
+const dbConfig = require("./db");
+
+console.log(process.env.DB)
 
 const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
   host: process.env.HOST,
