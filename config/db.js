349c0090f823c85ee6dc02207f23954a91471b64
Abdoulaye BERTE <abdouberte97@gmail.com>
2024-03-10 12:49:11 +0100

    update config db

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 // Configuration de la base de données MySQL
 module.exports = {
-  HOST: "us-cluster-east-01.k8s.cleardb.net",
-  USER: "be289f818608f1",
-  PASSWORD: "bacda800",
-  DB: "heroku_61772d7d84193e5",
+  HOST: "mysql-masteregel.alwaysdata.net",
+  USER: "350248",
+  PASSWORD: "MasterEgel2024!",
+  DB: "masteregel_db",
 };
 
 const { Sequelize } = require("sequelize");
