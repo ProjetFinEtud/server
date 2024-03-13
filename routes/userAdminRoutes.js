b245dc87eeb7f7ddf9c2c5144e368ecae5d40313
Abdoulaye BERTE <abdouberte97@gmail.com>
2024-03-13 03:27:52 +0100

    delete conversation

--- routes/userAdminRoutes.js
+++ routes/userAdminRoutes.js
@@ -17,6 +17,7 @@ router.put('/activate',authMiddlewareAdmin, userController.activeUser);
 router.get('/userDesactived',authMiddlewareAdmin, userController.usersDesactived);
 router.put('/activateRequestContact/:id', userController.activeRequestContact);
 router.delete('/deleteRequestContact/:id', userController.deleteRequestContact);
+
 router.delete('/userDeleteHisAccount', userController.userDeletehisAccount);
 router.put('/refusedRequestContact', userController.refusedRequestContact);
 router.put('/changelocalisation', userController.changeLocalisation);
