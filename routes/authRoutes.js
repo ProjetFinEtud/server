93f43b3b610dd373d922f6543e6e2131a8318a50
Berte Abdoulaye <abdouberte97@gmail.com>
2024-04-16 07:14:00 +0200

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.post('/login', authController.loginUser);
+router.get('/allMaster', authController.allMaster);
+router.get('/allPostes', authController.allPostes);
+router.get('/allDomaine', authController.allDomaines);
 router.post('/createstudent', authController.createUserStudent);
 router.post('/createxstudent', authController.createUserExStudent);
 router.get('/verifAuth', authController.verifAuth);
