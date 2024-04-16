cdd28a8d014e5661c44849a62358322708432319
Berte Abdoulaye <abdouberte97@gmail.com>
2024-04-16 10:39:00 +0200

    test

--- routes/posteRoutes.js
+++ routes/posteRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const posteController = require('../controllers/posteController');
-
+const authMiddlewareAdmin = require('../middleware/authAdminMiddleware.js');
 
 router.get('/allPostes', posteController.getAllPostesUser);
 
 router.get('/allPrePostes', posteController.getAllPrePostes);
 
-router.post('/createPreposte', posteController.createPreposte);
+router.post('/createPreposte',authMiddlewareAdmin, posteController.createPreposte);
 
-router.put('/updateposte/:id', posteController.updatePosteExStudent);
+router.put('/updateposte/:id',authMiddlewareAdmin, posteController.updatePosteExStudent);
 
-router.post('/createposte', posteController.createPosteExStudent);
+router.post('/createposte',authMiddlewareAdmin, posteController.createPosteExStudent);
 
-router.put('/updatePreposte/:id', posteController.updatePreposte);
+router.put('/updatePreposte/:id',authMiddlewareAdmin, posteController.updatePreposte);
 
 module.exports = router;
